460012c3a86ea8e7b8525132eebb124d27cb1bb5
John Rua <Jrua89@gmail.com>
2023-10-21 19:31:43 -0400

    first commit

---
+++ app/Http/Controllers/TodoListController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\ListItem;
+use Illuminate\Http\Request;
+
+class TodoListController extends Controller
+{
+    public function index() {
+     return view('welcome', ['listItems' => ListItem::where('is_complete', 0)->get()]);
+    }
+
+    public function markComplete($id) {
+
+        $listitem = ListItem::find($id);
+        $listitem->is_complete = 1;
+        $listitem->save();
+
+        return redirect('/');
+    }
+
+    public function saveItem(Request $request){
+
+        
+        $newlistitem = new ListItem;
+        
+        $newlistitem->name = $request->listItem;
+        $newlistitem->is_complete = 0;
+        $newlistitem->save();
+
+        return redirect('/');
+    }
+}
